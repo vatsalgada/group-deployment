6f40229a27b389104f3fdc0e3c8593dbce378d40
vatsalgada <contactvgada@gmail.com>
2024-11-18 03:24:16 +0530

    +config change

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { webpack, isServer }) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        bufferutil: false,
+        'utf-8-validate': false,
+      };
+      return config;
+    },
+  }
